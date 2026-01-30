d867c9f88fa785b0b81484f8d27a834e4b9036f8
adrnc <49120352+adrnc@users.noreply.github.com>
2026-01-30 12:50:24 +0100

    Implementation

--- src/final2/solve.py
+++ src/final2/solve.py
@@ -1,8 +1,8 @@
 from os import path
-from sys import argv
+from argparse import ArgumentParser
 from clingo import Control, Model
 
-def solve(asp_dir: str, input_files: str, show_supports: bool, show_precedents: bool, show_constraint: bool):
+def solve(asp_dir: str, input_files: str, show_supports: bool, show_precedents: bool):
     ctl1 = Control(["--enum-mode=cautious"])
     ctl1.load(f"{asp_dir}/step1.lp")
 
@@ -19,16 +19,14 @@ def solve(asp_dir: str, input_files: str, show_supports: bool, show_precedents: 
 
     ctl1.solve(on_model=on_model_step1)
 
-    show = []
+    show = ["constraint/2"]
 
     if show_supports:
         show.append("min_supports/3")
     if show_precedents:
         show.append("precedent/3")
-    if show_constraint:
-        show.append("constraint/2")
 
-    ctl2 = Control(["--project", f"--show {",".join(show)}"])
+    ctl2 = Control(["--project", "--show", ",".join(show)])
     ctl2.load(f"{asp_dir}/step2.lp")
 
     with ctl2.backend() as backend:
@@ -37,20 +35,17 @@ def solve(asp_dir: str, input_files: str, show_supports: bool, show_precedents: 
             backend.add_rule([atom_id])
 
     ctl2.ground([("base", [])])
-    ctl2.solve(on_model=lambda m: print(f"Answer: {m}"))
+    ctl2.solve(on_model=lambda m: print(m))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog='Model',
-        description='An implementation of the Reduction Model for incomplete and multi-precedent cases')
+    parser = ArgumentParser(
+        prog="Model",
+        description="An implementation of the Reduction Model for incomplete and multi-precedent cases")
 
-    parser.add_argument('filename')
-    parser.add_argument('-s', '--show-supports')
-    parser.add_argument('-p', '--show-precedents')
-    parser.add_argument('-c', '--show-constraint')
+    parser.add_argument("-s", "--show-supports", action="store_true", help="Show the minimal supporting thresholds for dimensions")
+    parser.add_argument("-p", "--show-precedents", action="store_true", help="Show the precedent cases in the output")
+    parser.add_argument("inputfiles", nargs="+", help="The input files in .lp format containing the case base and the focus case")
 
-    if len(argv) <= 1:
-        print("No inputs file provided")
-        exit(1)
+    args = parser.parse_args()
 
-    solve(f"{path.dirname(__file__)}/asp", argv[1:])
+    solve(f"{path.dirname(__file__)}/asp", args.inputfiles, args.show_supports, args.show_precedents)
